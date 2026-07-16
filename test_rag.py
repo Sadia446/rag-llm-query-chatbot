4c2d17b7bd4e6ebbf2af94b7230afb722c20d282
Sadia Noreen <sadianoreen602@gmail.com>
2026-07-16 22:46:42 +0500

    Fix in Ticket to Ride question

--- test_rag.py
+++ test_rag.py
@@ -18,7 +18,7 @@ def test_monopoly_rules():
 
 def test_ticket_to_ride_rules():
     assert query_and_validate(
-        question="How many points does the longest continuous train get in Ticket to Ride? (Answer with the number only)",
+        question="How many Points does the longest continuous train get in Ticket to Ride? (Answer with the number only)",
         expected_response="10 points",
     )
 
